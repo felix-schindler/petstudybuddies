5c1db00864c2943e82e984c6b6d2fe433bbe6594
Felix Schindler <fs146@hdm-stuttgart.de>
2021-04-28 11:30:52 +0200

    account get/set user

--- Idea-Projekt/src/main/java/de/hdm_stuttgart/mi/PetStudyBuddies/Core/Account.java
+++ Idea-Projekt/src/main/java/de/hdm_stuttgart/mi/PetStudyBuddies/Core/Account.java
@@ -6,7 +6,7 @@ import de.hdm_stuttgart.mi.PetStudyBuddies.Models.*;
  * Account - This class holds all of the user specific data.
  */
 public class Account {
-    private User user;
+    private static User user;
     private Pet pet;
     private Calendar calendar;
     private Note[] notes;
@@ -14,12 +14,21 @@ public class Account {
     private Studies studies;
 
     public Account() {
+        user = null;        // no user is logged in
         // TODO get pet, calendar, notes, todolists and studies out of some kind of storage (database, json, sqlite)
     }
 
     boolean deleteAccount() {
         // TODO Delete everything (Constrainteigenschaften -> DELETE)
         return false;
+    }
+
+    public static void setUser(User user) {
+        Account.user = user;
+    }
+
+    public static User getUser() {
+        return user;
     }
 
     boolean createPet(String name) {
