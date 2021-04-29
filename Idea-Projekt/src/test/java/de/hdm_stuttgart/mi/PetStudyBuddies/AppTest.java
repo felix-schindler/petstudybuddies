8ac539e36aaf8ad0eb4e2cb6e077f73c1e74a87c
Felix Schindler <fs146@hdm-stuttgart.de>
2021-04-29 11:42:11 +0200

    add basic unit test

--- Idea-Projekt/src/test/java/de/hdm_stuttgart/mi/PetStudyBuddies/AppTest.java
+++ Idea-Projekt/src/test/java/de/hdm_stuttgart/mi/PetStudyBuddies/AppTest.java
@@ -1,5 +1,7 @@
 package de.hdm_stuttgart.mi.PetStudyBuddies;
 
+import de.hdm_stuttgart.mi.PetStudyBuddies.Core.Auth;
+import de.hdm_stuttgart.mi.PetStudyBuddies.Models.User;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,5 +15,25 @@ public class AppTest {
     @Test
     public void testApp() {
         Assert.assertTrue( true );
+    }
+
+    /**
+     * Test the Auth.login Method
+     */
+    @Test
+    public void testLogin() {
+        Assert.assertNull(Auth.login("test", "test"));
+        Assert.assertNotNull(Auth.login("fs146@hdm-stuttgart.de", "test"));
+    }
+
+    /**
+     * Test the Auth.register Method
+     */
+    @Test
+    public void testRegister() {
+        Assert.assertTrue(Auth.register("fs146@hdm-stuttgart.de", "felix", "test"));
+        Assert.assertTrue(Auth.register("guenther.oettinger@parlament.eu", "guenther", "12345"));
+        Assert.assertTrue(Auth.register("kriha@hdm-stuttgart.de", "kriha", "r6D84=xt*UCvpJBa"));
+        Assert.assertTrue(Auth.register("mb365@hdm-stuttgart.de", "xX_Lover_Xx", "MeinNeuesSichersPasswort"));
     }
 }
