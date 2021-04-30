ed59c3ab5a863112bd7767399f8593c01f474c57
Felix Schindler <fs146@hdm-stuttgart.de>
2021-04-30 09:19:00 +0200

    Update Model.java

--- Idea-Projekt/src/main/java/de/hdm_stuttgart/mi/PetStudyBuddies/Core/DB/Model.java
+++ Idea-Projekt/src/main/java/de/hdm_stuttgart/mi/PetStudyBuddies/Core/DB/Model.java
@@ -3,8 +3,15 @@ package de.hdm_stuttgart.mi.PetStudyBuddies.Core.DB;
 public abstract class Model {
     private int ID;
 
+    protected Model(int ID) {
+        this.ID = ID;
+    }
+
     public int getID() {
         return ID;
+    }
+    public void setID(int ID) {
+        this.ID = ID;
     }
 
     public abstract String getTable();
