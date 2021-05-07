2c838a1a81c1b3947d1fd2762ce68bbe4c798b7e
kikibeh <kb136@hdm-stuttgart.de>
2021-05-07 13:02:39 +0200

    Minor changes in DeleteQuery Class and UpdateQueryTest
    Tests now working

--- Idea-Projekt/src/main/java/de/hdm_stuttgart/mi/PetStudyBuddies/Core/DB/DeleteQuery.java
+++ Idea-Projekt/src/main/java/de/hdm_stuttgart/mi/PetStudyBuddies/Core/DB/DeleteQuery.java
@@ -1,13 +1,25 @@
 package de.hdm_stuttgart.mi.PetStudyBuddies.Core.DB;
 
-public class DeleteQuery {
+public class DeleteQuery extends Query{
     private StringBuilder query = new StringBuilder();
 
     public DeleteQuery(String table, String where) {
+        BuildQuery(table, where);
+        SetQueryString(query.toString());
+    }
+    public DeleteQuery(String table, String where, boolean run) {
+        BuildQuery(table, where);
+        if (run) {
+            SetQueryString(query.toString());
+        }
+    }
+    
+    public void BuildQuery(String table, String where) {
         query.append("DELETE FROM " + table);
         if(where != null){
             query.append(" WHERE " + where);
         }
         query.append(";");
     }
+
 }
--- Idea-Projekt/src/test/java/de/hdm_stuttgart/mi/PetStudyBuddies/Core/DB/UpdateQueryTest.java
+++ Idea-Projekt/src/test/java/de/hdm_stuttgart/mi/PetStudyBuddies/Core/DB/UpdateQueryTest.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 public class UpdateQueryTest {
     @Test
     public void testUpdateSimple() {
-        Assert.assertEquals("UPDATE User SET EMail='mail@example.com';", new UpdateQuery("User", "EMail", "mail@example.com", null).GetQueryString());
-        Assert.assertEquals("UPDATE Note SET Title='Fragen an Kriha';", new UpdateQuery("Note", "Title", "Fragen an Kriha", null).GetQueryString());
-        Assert.assertEquals("UPDATE User SET EMail='fs146@hdm-stuttgart.de';", new UpdateQuery("User", "EMail", "fs146@hdm-stuttgart.de", null).GetQueryString());
+        Assert.assertEquals("UPDATE User SET EMail = 'mail@example.com';", new UpdateQuery("User", "EMail", "mail@example.com", null).GetQueryString());
+        Assert.assertEquals("UPDATE Note SET Title = 'Fragen an Kriha';", new UpdateQuery("Note", "Title", "Fragen an Kriha", null).GetQueryString());
+        Assert.assertEquals("UPDATE User SET EMail = 'fs146@hdm-stuttgart.de';", new UpdateQuery("User", "EMail", "fs146@hdm-stuttgart.de", null).GetQueryString());
     }
 
     @Test
     public void testUpdateWhere() {
         Assert.assertEquals("UPDATE Pet SET Name = 'Inge' WHERE UserID=1;", new UpdateQuery("Pet", "Name", "Inge", "UserID=1").GetQueryString());
         Assert.assertEquals("UPDATE User SET EMail = 'fs146@hdm-stuttgart.de' WHERE ID=1;", new UpdateQuery("User", "EMail", "fs146@hdm-stuttgart.de", "ID=1").GetQueryString());
-        Assert.assertEquals("UPDATE Note SET Title = 'Hier könnte Ihr Titel stehen' WHERE ID=1;", new UpdateQuery("Note", "title", "Hier könnte Ihr Titel stehen", "ID=1").GetQueryString());
+        Assert.assertEquals("UPDATE Note SET Title = 'Hier könnte Ihr Titel stehen' WHERE ID=1;", new UpdateQuery("Note", "Title", "Hier könnte Ihr Titel stehen", "ID=1").GetQueryString());
     }
 
     @Test
