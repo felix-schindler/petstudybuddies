95dfe094bef480ab196c26d315ec91d45e566c1e
Felix Schindler <fs146@hdm-stuttgart.de>
2021-05-07 11:51:52 +0200

    add variables; constructor; save()

--- Idea-Projekt/src/main/java/de/hdm_stuttgart/mi/PetStudyBuddies/Models/Lecture.java
+++ Idea-Projekt/src/main/java/de/hdm_stuttgart/mi/PetStudyBuddies/Models/Lecture.java
@@ -1,17 +1,40 @@
 package de.hdm_stuttgart.mi.PetStudyBuddies.Models;
 
+import de.hdm_stuttgart.mi.PetStudyBuddies.Core.DB.Query;
+import de.hdm_stuttgart.mi.PetStudyBuddies.Core.DB.SelectQuery;
 import de.hdm_stuttgart.mi.PetStudyBuddies.Core.Model;
 import de.hdm_stuttgart.mi.PetStudyBuddies.Core.Shareable;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Lecture extends Model implements Shareable {
     private String title;
+    int ECTS;
+    int MajorID;
 
     public Lecture(int ID) {
         super(ID);
+        try {
+            ResultSet lecture = new Query("SELECT * FROM Lecture WHERE ID=" + ID).Fetch();
+            title = lecture.getString("Title");
+            ECTS = lecture.getInt("ECTS");
+            MajorID = lecture.getInt("MajorID");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public boolean save() {
+        return new Query("UPDATE Note SET Title='"+title+"', ECTS="+ECTS+", MajorID="+MajorID+" WHERE ID="+getID()+";").Success();
     }
 
     public boolean share(int ID) {
         return false;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public String getUserID() {
