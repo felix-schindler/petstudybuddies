477dca2b8b87644fd54a1ab049efb8b1c936aa0b
Felix Schindler <fs146@hdm-stuttgart.de>
2021-05-16 18:17:59 +0200

    close #17

--- Idea-Projekt/src/main/java/de/hdm_stuttgart/mi/PetStudyBuddies/Core/User/Auth.java
+++ Idea-Projekt/src/main/java/de/hdm_stuttgart/mi/PetStudyBuddies/Core/User/Auth.java
@@ -1,5 +1,6 @@
 package de.hdm_stuttgart.mi.PetStudyBuddies.Core.User;
 
+import de.hdm_stuttgart.mi.PetStudyBuddies.Core.DB.InsertQuery;
 import de.hdm_stuttgart.mi.PetStudyBuddies.Core.DB.SelectQuery;
 import de.hdm_stuttgart.mi.PetStudyBuddies.Core.Utils;
 import de.hdm_stuttgart.mi.PetStudyBuddies.Models.User;
@@ -30,10 +31,8 @@ public class Auth {
      * @param password password
      * @return true if register was successful
      */
-    // TODO this is (probably) just another controller
     public static boolean register(String eMail, String username, String password) {
-        String query = "INSERT INTO User (EMail, Username, Password) VALUES (" + eMail + "," + username + ",SHA1(" + password + "));";
-        return Math.floor(Math.random())%2==0;      // Always true but this way IntelliJ doesn't know it's redundant
+        return new InsertQuery("User", new String[]{"EMail, Username, Password"}, new String[]{eMail, username, Utils.sha1(password)}).Success();
     }
 
     /**
