03220d4c41c2a61aa24be0fb07dd8b48730a8997
kikibeh <kb136@hdm-stuttgart.de>
2021-05-20 15:42:23 +0200

    Changes in DeleteQueryTest and log-file

--- Idea-Projekt/src/test/java/de/hdm_stuttgart/mi/PetStudyBuddies/CoreTest/DBTest/DeleteQueryTest.java
+++ Idea-Projekt/src/test/java/de/hdm_stuttgart/mi/PetStudyBuddies/CoreTest/DBTest/DeleteQueryTest.java
@@ -22,15 +22,24 @@ public class DeleteQueryTest {
     @Test
     public void testDeleteSeveralTables() throws SQLException {
        new InsertQuery("ToDoList", new String[]{"UserID","Title"}, new String[]{"1","TestNote2"}, true).GetQueryString();
-       //new InsertQuery("ToDoListShare", new String[]{"UserID","ToDoListID"}, new String[]{"2","9"}, true).GetQueryString();
         ResultSet ToDoListID=  new SelectQuery("ToDoList","ID","Title='TestNote2'",null,null,true).fetchAll();
         ArrayList<Integer> IDs = new ArrayList<Integer>();
         while (ToDoListID.next()){
             IDs.add(ToDoListID.getInt(1));
         }
+        for( int i:IDs){
+            new InsertQuery("ToDoListShare", new String[]{"UserID","ToDoListID"}, new String[]{"2",Integer.toString(i)}, true).GetQueryString();
+        }
         System.out.println(IDs);
       // Assert.assertEquals("DELETE FROM ToDoList WHERE Title='TestNote2';",new DeleteQuery("ToDoList","Title='TestNote2'").GetQueryString());
       // Assert.assertNull("", new SelectQuery("ToDoList","*","Title='TestNote2'",null,null,true));
-      //  Assert.assertNull("", new SelectQuery("ToDoListShare","*","Title='TestNote2'",null,null,true));
+        ArrayList<String> IDsShare = new ArrayList<>();
+        for( int i:IDs){
+            String selectedID= new SelectQuery("ToDoListShare","ID","ToDoListID='"+i+"'",null,null,true).fetch();
+            IDsShare.add(selectedID);
+            new DeleteQuery("ToDoListShare","ID = "+selectedID+"",true);
+            Assert.assertEquals("",new SelectQuery("ToDoListShare","ID","ToDoListID='"+i+"'",null,null,true).fetch());
+        }
+
     }
 }
