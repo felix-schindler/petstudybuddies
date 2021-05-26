2dc6e17d5dd924f25fc004436f3e1f6596bae044
Felix Schindler <fs146@hdm-stuttgart.de>
2021-05-26 11:18:26 +0200

    Delete WelcomeView.java

--- Idea-Projekt/src/main/java/de/hdm_stuttgart/mi/PetStudyBuddies/Views/WelcomeView.java
+++
@@ -1,9 +0,0 @@
-package de.hdm_stuttgart.mi.PetStudyBuddies.Views;
-
-import de.hdm_stuttgart.mi.PetStudyBuddies.PetStudyBuddies;
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class WelcomeView extends Stage {
-    PetStudyBuddies.setStage(this);
-}
